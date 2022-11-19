8a17b6b6859302bad651d994f0be8274010220d2
Linus Reimertz <linus.reimertz2@gmail.com>
2022-11-19 20:28:26 +0100

    Updates

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (e) => {
+  console.log(
+    "service worker has been installed"
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  console.log(
+    "service worker has been activated"
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log("fetch event", e);
+});
