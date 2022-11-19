3da2fc08051d4494c05116e69ecad5b07ef3b40d
Linus Reimertz <linus.reimertz2@gmail.com>
2022-11-19 15:28:20 +0100

    PWA stuff added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import "./app.postcss";
 import App from "./App.svelte";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then((reg) =>
+      console.log(
+        "service worker registered",
+        reg
+      )
+    )
+    .catch((err) =>
+      console.log(
+        "service worker not registered",
+        err
+      )
+    );
+}
+
 const app = new App({
-  target: document.getElementById("app"),
+  target: document.getElementById("app")
 });
 
 export default app;
